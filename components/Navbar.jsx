08538b9032b6822955035e035135bd09060fc34d
Prince Singh <marvelousprince012233@gmail.com>
2022-08-15 16:27:36 +0530

    better img quality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <div className='fixed bottom-0 w-full z-50 bg-white shadow-[0_-5px_10px_0] shadow-[#40404018] flex justify-center items-center space-x-8 py-4 rounded-t-3xl'>
       <Link href='/'>
-        <a><img src='/google_playstore.png'></img></a>
+        <a><img className='h-12' src='/google_playstore.png'></img></a>
       </Link>
       <Link href='/'>
-        <a><img src='/appstore.png'></img></a>
+        <a><img className='h-12' src='/appstore.png'></img></a>
       </Link>
     </div>
   )
